c3f1cf7b15dd5b87b67c953c6ea261e7c37ccd27
coderILKIN <tu758403c@code.edu.az>
2023-10-27 18:32:01 +0400

    task add

---
+++ Havanin tempraturu/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Havanin_tempraturu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1ci hal */
+            //Console.WriteLine("---- Havanin Tempiratorunu Yazin ----");
+            //int t = Convert.ToInt32(Console.ReadLine());
+
+            //if (t < 15)
+            //{
+            //    Console.WriteLine("Hava Soyuqdur");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Hava istidir");
+            //}
+
+
+
+            /* 2ci hal */
+
+
+            //Console.WriteLine("---- Havanin Tempiratorunu Yazin ----");
+
+            //int t = Convert.ToInt32(Console.ReadLine());
+
+            //if (t == 0)
+            //{
+            //    Console.WriteLine("Hava saxtalidir");
+            //}
+
+            //else if (t < -20)
+            //{
+            //    Console.WriteLine("Bayira cixmaq sizin ucun cox tehlukelidir");
+            //}
+
+            //else if (t < 0)
+            //{
+            //    Console.WriteLine("Havada saxta getdikce artir");
+            //}
+
+            //else if (t < 15)
+            //{
+            //    Console.WriteLine("Hava Soyuqdur");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Hava istidir");
+            //}
+
+
+
+            /* 3cu hal */
+
+            //Console.WriteLine("---- Havanin Tempiratorunu Yazin ----");
+            //int t;
+
+            //for (int i = 1; i > 0; i++)
+            //{
+            //   t = Convert.ToInt32(Console.ReadLine());
+            //    if (t == 0)
+            //    {
+            //        Console.WriteLine("Hava saxtalidir");
+            //    }
+
+            //    else if (t < -20)
+            //    {
+            //        Console.WriteLine("Bayira cixmaq sizin ucun cox tehlukelidir");
+            //    }
+
+            //    else if (t < 0)
+            //    {
+            //        Console.WriteLine("Havada saxta getdikce artir");
+            //    }
+
+            //    else if (t < 15)
+            //    {
+            //        Console.WriteLine("Hava Soyuqdur");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("Hava istidir");
+            //    }
+
+
+            //    Console.WriteLine("---Yeni tempirator yazin---");
+            //}
+
+
+        }
+    }
+}
